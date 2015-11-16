f6aacb596c0c06d5e5f89683afbf195f664ca77e
Isaac Cocar <isaac.cocar@gmail.com>
2015-11-16 12:41:43 -0400

    changed home to index

---
+++ www/app/index/index.js
@@ -0,0 +1,24 @@
+angular.module('app.index', [])
+
+.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  $stateProvider
+  .state('index', {
+    url: '/',
+    templateUrl: 'app/index/index.html',
+    controller: 'IndexCtrl'
+  });
+})
+
+.controller('IndexCtrl', ['$scope', '$state', '$ionicPopup', '$window', '$ionicHistory', function ($scope, $state, $ionicPopup, $window, $ionicHistory) {
+  $scope.continueGame = function () {
+    if (angular.equals({}, JSON.parse($window.localStorage.game || '{}'))) {
+      $ionicPopup.alert({
+        title: 'There is no saved game',
+        buttons: [{text: 'OK', type: 'button-dark'}]
+      });
+    }
+    else {
+      $state.go('game');
+    }
+  };
+}]);
